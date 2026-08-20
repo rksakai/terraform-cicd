29cd169f758abf30a21aaef37eee43d19e0fa9a4
Rodrigo <profrodrigo.sakai@fiap.com.br>
2026-08-20 22:51:15 +0000

    Alteração do nome do container

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,7 +38,7 @@ resource "azurerm_storage_account" "storage" {
 }
 
 resource "azurerm_storage_container" "medallion-bronze" {
-  name                  = "bronze"
+  name                  = "bronze-v1"
   storage_account_name  = azurerm_storage_account.storage.name
   container_access_type = "private"
 }
